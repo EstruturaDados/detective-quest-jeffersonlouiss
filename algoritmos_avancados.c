27808f15e073f8e7b854139c49493cff16fab159
JeffersonLouis <42342922+jeffersonlouiss@users.noreply.github.com>
2025-11-16 01:49:28 +0000

    implementado nivel novato do desafio

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSTRING 50
+#define MAXFILA 14
 
 // Desafio Detective Quest
 // Tema 4 - Árvores e Tabela Hash
 // Este código inicial serve como base para o desenvolvimento das estruturas de navegação, pistas e suspeitos.
 // Use as instruções de cada região para desenvolver o sistema completo com árvore binária, árvore de busca e tabela hash.
+typedef struct Sala
+{
+    char nome[MAXSTRING];
+    struct Sala* esquerda;
+    struct Sala* direita;
+    int depth;
+}Sala;
 
+typedef struct Fila{
+    Sala* comodos[MAXFILA];
+    int inicio;
+    int fim;
+    int total;
+}Fila;
+
+void conectarSalas(Sala *raiz);
+Sala* criarSala(char* nome);
+Sala* explorarSalas(Sala *s, char direction);
+Sala* dequeue(Fila *f);
+void enqueue(Fila *f, Sala *s);
+void iniciarFila(Fila *f);
+void freeArvore(Sala *s);
+
+char* const comodos[] = {"Corredor","Sala","Quarto","Banheiro","Suite","Cozinha","Garagem","Varanda","Sotao","Biblioteca","Sauna","Churrasqueira","Quato Visita"};
 int main() {
 
     // 🌱 Nível Novato: Mapa da Mansão com Árvore Binária
@@ -17,6 +45,40 @@ int main() {
     // - Exiba o nome da sala a cada movimento.
     // - Use recursão ou laços para caminhar pela árvore.
     // - Nenhuma inserção dinâmica é necessária neste nível.
+    Sala* raiz =  criarSala("Entrada");
+    conectarSalas(raiz);
+    char direction;
+    Sala *salaAtual = raiz;
+    do{
+        printf("Voce esta na comodo %s\n", salaAtual->nome);
+        printf("d -> ir para a Direita\n");
+        printf("e -> ir para a Esquerda\n");
+        printf("s -> Sair\n");
+        printf("Escolha uma Direcao: ");
+        scanf("%c", &direction);
+        getchar();
+
+        if(direction == 'd')
+        {
+            salaAtual = explorarSalas(salaAtual, 'r');
+            if(salaAtual == NULL)
+            {
+                printf("Voce atingiu a ultima sala\n");
+                break;
+            }
+        }
+        else if(direction == 'e')
+        {
+            salaAtual = explorarSalas(salaAtual, 'l');
+            if(salaAtual == NULL)
+            {
+                printf("Voce atingiu a ultima sala\n");
+                break;
+            }
+        }
+
+    }while(direction != 's');
+    freeArvore(raiz);
 
     // 🔍 Nível Aventureiro: Armazenamento de Pistas com Árvore de Busca
     //
@@ -44,4 +106,107 @@ int main() {
 
     return 0;
 }
+void iniciarFila(Fila *f)
+{
+    f->inicio = 0;
+    f->fim = 0;
+    f->total = 0;
+}
+void enqueue(Fila *f, Sala *s){
+    f->comodos[f->fim] = s;
+    f->fim = (f->fim +1);
+    f->total++;
+}
 
+Sala* dequeue(Fila *f)
+{
+    Sala* deqSala = f->comodos[f->inicio + 1];
+    f->inicio = (f->inicio + 1);
+    f->total--;
+    return deqSala;
+}
+
+
+Sala* criarSala(char* nome)
+{
+    Sala* novaSala = malloc(sizeof(struct Sala));
+    if(novaSala == NULL) return NULL;
+    strcpy(novaSala->nome, nome);
+    novaSala->direita = NULL;
+    novaSala->esquerda = NULL;
+    return novaSala;    
+}
+/**
+ * Esta função conectarSalas() percorre a lista nomes de cômodos e cria novas salas para cada um deles.
+ * Para manter a organização em forma de árvore binária, é utilizada uma Fila (FIFO) que garante
+ * que as salas sejam conectadas em ordem de nível (level-order), semelhante ao preenchimento de
+ * uma árvore binária completa.
+ *
+ * Funcionamento:
+ * - A sala raiz é colocada na fila.
+ * - Para cada novo cômodo, cria-se uma nova Sala e ela é enfileirada.
+ * - O elemento no início da fila (atual) é preenchido primeiro à esquerda, depois à direita.
+ * - Quando ambos os filhos de 'atual' estão ocupados, retira-se o próximo elemento da fila,
+ *   garantindo a expansão em largura.
+ *
+ * O uso da Fila é essencial para manter a estrutura organizada e evitar conexões incorretas,
+ * permitindo que a árvore de salas seja construída de forma ordenada e previsível.
+ *
+ */
+
+void conectarSalas(Sala *raiz)
+{
+    int total = sizeof(comodos) / sizeof(comodos[0]);
+    Sala* atual = raiz;
+    Fila f;
+    depth += 1;
+    iniciarFila(&f);
+    enqueue(&f, raiz);
+    for(int i = 0; i < total; i++)
+    {
+        Sala* novoComodo = criarSala(comodos[i]);
+        enqueue(&f, novoComodo);
+        if(atual->esquerda == NULL)
+        {
+            atual->esquerda = novoComodo;
+        }
+        else if(atual->direita == NULL)
+        {
+            atual->direita = novoComodo;
+        }
+        
+        else {
+            atual = dequeue(&f);
+            atual->esquerda = novoComodo;
+        }
+    }
+}
+
+Sala* explorarSalas(Sala *s, char direction)
+{
+    if(direction == 'l' && s->esquerda != NULL)
+    {
+        return s->esquerda;
+    }
+    if(direction == 'r' && s->direita != NULL)
+    {
+        return s->direita;
+    }
+    
+    return NULL;
+    
+}
+
+void freeArvore(Sala *s)
+{
+    if(s->esquerda)
+    {
+        freeArvore(s->esquerda);
+    }
+    if(s->direita)
+    {
+        freeArvore(s->direita);
+    }
+    free(s);
+    return;
+}
